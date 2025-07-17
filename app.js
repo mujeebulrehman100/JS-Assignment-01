e5af286e83f9e93080b9f0d801d74c35586cdb9e
mujeebleghari100 <mujeebleghari45@gmail.com>
2025-07-17 14:56:27 -0700

    JS Assignment 01

---
+++ app.js
@@ -0,0 +1,215 @@
+     //Chapter#01 Alert
+
+//Q#1
+//alert("WelCome to my Website");
+
+//Q#2
+//alert("Welcome to Js Land \n" + "Happy Coding!");
+ 
+//Q#3
+//alert("Welcome to Js Land");
+
+
+//Q#5
+//var message=("Hello.. I can run JS through my web browser's console!");
+//alert (message);
+//console.log(message);
+
+//Q#4
+//alert("Happy Coding!");
+
+
+        //Chapter#02 Variable & Strings
+
+//Q#1
+//var UserName="Mujeeb";
+//alert(UserName);
+
+//Q#2
+//var Age="15 years old"
+//alert(Age);
+
+//Q#3
+//var Certified="Certified Mobile App Devolpment";
+//alert(Certified);
+
+//Q#4
+//alert("PIZZA \n" + "PIZZ \n" + "PIZ \n" + "PI \n" + "P" );
+
+//Q#5
+//var Email="My Email Address is mujeebleghari45@gmail.com";
+//alert(Email);
+
+//Q#6
+//var About="I am trying to learn from the Book A Smarter Way to Learn JavaScript";
+//alert(About);
+
+//Q#6
+//var write="Yah! I can write HTML content through JavaScript";
+//document.write(write);
+
+//Q#7
+ //var Design=("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+ //alert(Design);
+ //document.write(Design)
+        //Chapter#03 Variable & Number
+
+//Q#1
+ //var Age="I am 15 years old";
+ //alert(Age);
+
+//Q#2
+//var Visit="You Have Visited this site 46 times"
+//alert(Visit)
+
+//Q#3
+//var birth="My birth year is 1990 ";
+//document.write(birth);
+
+//Q#4
+//var productname="Shirt";
+//var producttitle="  It's Cuton Clothes   ";
+//var productquantity=  12;
+//var store="   www/online.store";
+
+//document.write(productname  +   producttitle +   productquantity +   store);
+
+            //Chapter#04 Variable ligeal&ilegal
+
+//Q#1
+//var camelCase="   it Is A Camel Case Example   " 
+//document.write(camelCase);
+
+//Q#2
+//var snake_case$="  it is a snake_case Example  "
+//document.write(snake_case$);
+
+//Q#3
+//var PascalCase="  It Is A Pascal Case Example  "
+//document.write(PascalCase);
+
+
+
+
+
+
+
+
+
+//                                   chapter : 5  MATH EXPRESSIONS
+
+
+
+
+      //                                         Qno 1
+      //  ***--------------------------------(        )-------------------------------------*
+
+// var num1=3;
+// var num2=5;
+// var result=(num1 + num2)
+// document.write("sum of 3 and 5 is "+result)
+
+      //                                          Qno2
+        //  ***--------------------------------(        )-------------------------------------*
+
+//                 substraction
+// var num1 = 3;
+// var num2 = 5;
+// var result=(num1-num2)
+// document.write("substraction of 3 and 5 is "+ result)
+
+
+//              multiplication   
+// var num1=3;
+// var num2=5;
+// var result =(num1*num2)
+// document.write(" multiplication of 3 and 5 is " + result)
+        
+
+//               Division        
+// var num1 = 3;
+// var num2 = 5;
+// var result = (num1 / num2)
+// document.write(" division of 3 and 5 is " + result)
+ 
+
+
+//               Modulus
+// var num1 = 6;
+// var num2 = 3;
+// var result = (num1 % num2)
+// document.write(" modulus of 6 and 3 is " + result)
+
+
+
+
+
+//                                            Qno : 3
+//      ***--------------------------------(01 - 02)-------------------------------------*
+
+// var num1;
+
+
+// document.write("Value after variable declaration is:" + num1 + "<br>")
+
+//     ***--------------------------------(02 - 03)-------------------------------------*
+
+// var num1=10;
+
+// document.write("Initial value:"+num1 + "<br>")
+
+//     ***--------------------------------(04 - 05)-------------------------------------*
+// var num1=5;
+// var result= ++num1;
+// document.write(" Value after increment is:"+ num1 + "<br>")
+   
+
+
+//     ***--------------------------------(06 - 07)-------------------------------------*
+
+
+// var num1=6;
+// var result= num1 += 7;
+// document.write("Value after addition is:"+ result +"<br>") 
+
+
+//     ***--------------------------------(08 - 09)-------------------------------------*
+// var num1=13;
+// var result = --num1;
+// document.write("Value  after decrement is:"+result+"<br>")
+
+
+
+//     ***--------------------------------(10 - 11)-------------------------------------*
+
+// var num1=12;
+// var num2=3;
+// var result = num1 % num2;
+// document.write("The remainder is :"+result+"<br>")
+
+
+//                                            Qno : 4
+//      ***--------------------------------(01)-------------------------------------*
+// var oneticket=600;
+// var fiveticket=5;
+// var Total= oneticket * fiveticket;
+// document.write("Total cost to buy 5 tickets to a movie is "+ Total + "PKR" + "<br>")
+
+
+//                                            Qno : 5
+//      ***--------------------------------(01)-------------------------------------*
+
+// var table = 2;
+// // 2 x 1 = 2
+// // document.write(table + " x " + multiple + " = " + result)
+
+// for (var multiple = 1; multiple <= 10; multiple++) {
+
+//   var result = table * multiple;
+
+//   document.write("</br>" + table + " x " + multiple + " = " + result )
+// }
+
+
+
+
